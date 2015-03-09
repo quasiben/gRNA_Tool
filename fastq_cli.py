2954b07d3c7e8c9c05802a0624ca21b36d52471f
root <root@ip-10-169-120-151.ec2.internal>
2015-03-09 13:37:21 +0000

    fixing file collection process and adding in dask support

--- fastq_cli.py
+++ fastq_cli.py
@@ -23,6 +23,8 @@ def parse_args():
                         help="read directory of files.  "
                              "Files must be of match: *R1*.gz, *R2*.gz",
                         )
+    parser.add_argument("--parallel", action="store_true", 
+		        help="multicore support with Dask")
     # parser.add_argument("-o", "--output",
     #                     default="output.csv",
     #                     type=str,
@@ -42,6 +44,7 @@ def process_gRNA(forward_path, reverse_path):
       None: write to disk with fwd_name.csv
 
     """
+    print "Processing Files: ", forward_path, reverse_path
 
     wtlib_dict = wtlib_lib.get_wtlib_index()
     counter_dict = defaultdict(int)
@@ -66,7 +69,8 @@ def process_gRNA(forward_path, reverse_path):
     # output file clean up :)
     output_file = os.path.basename(forward_path)  # get file name only
     output_file = output_file.split('.')[0]  # remove extensions
-    output_file = re.sub("_R1.*", "", output_file)  # section out _R1*
+    #output_file = re.sub("_R1.*", "", output_file)  # section out _R1*
+    output_file = output_file.replace("_R1", "")  # section out _R1*
     output_file = output_file+".csv"
 
     with open(output_file, 'w') as csvfile:
@@ -91,23 +95,23 @@ def main():
         sys.exit("Can define both dir and file options.  Please choose one")
 
     elif args.dir:
-        files = glob(args.dir + "/*.gz")
+        files = glob(args.dir + "/*R1*.gz")
         files.sort()
 
     else:
         fwd_file = args.fwd
         rev_file = args.rev
         files = [fwd_file, rev_file]
+    
+    files = [(f, f.replace("_R1", "_R2")) for f in files] 
 
-    def pairwise(iterable):
-        "s -> (s0,s1), (s2,s3), (s4, s5), ..."
-        a = iter(iterable)
-        return izip(a, a)
-
-    for f, r in pairwise(files):
-        print "Processing Files: ", f, r
-
-        process_gRNA(f, r)
+    if args.parallel:
+        from dask.bag import Bag
+        b = Bag.from_sequence(files)
+        list(b.map(lambda f, r: process_gRNA(f,r)))
+    else:
+        for f,r  in files:
+            process_gRNA(f, r)
 
 if __name__ == '__main__':
     main()
